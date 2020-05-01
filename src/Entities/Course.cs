c6768fc28f0a13d1240b250f727b633b461ae572
Talen Fisher <talen.fisher@cythral.com>
2020-05-01 09:07:49 -0500

    BDC-18 Add LiveLink Column

--- src/Entities/Course.cs
+++ src/Entities/Course.cs
@@ -10,6 +10,8 @@ namespace BrekkeDanceCenter.Classes.Entities {
 
         public string Name { get; set; }
 
+        public string LiveLink { get; set; }
+
         public ICollection<Class> Classes { get; set; }
     }
 }
---
+++ src/Migrations/20200501140449_AddLiveLink.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BrekkeDanceCenter.Classes.Migrations
+{
+    public partial class AddLiveLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LiveLink",
+                table: "Courses",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LiveLink",
+                table: "Courses");
+        }
+    }
+}
